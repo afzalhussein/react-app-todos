0e8b98849be8d0737c9f10dafc40de0041251dab
Syed Afzal Hussain <afzalhussein@hotmail.com>
2024-08-10 03:14:05 -0400

    Added shopping cart with add item and test data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
+import Product from './components/Product/Product';
 import './App.css';
-import Link from './components/Link';
-import Title from './components/Title';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title alt="Edit">Edit <code>src/App.js</code> and save to reload.</Title>
-        <Link href="https://reactjs.org" title="Click to visit" alt="reactjs.org" className="code" target="_top">Link React</Link>
-      </header>
-    </div>
+    <Product />
   );
 }
 
