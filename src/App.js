2f44dcc922523f2453f2ef48de3e0797ae7efe03
Syed Afzal Hussain <afzalhussein@hotmail.com>
2024-08-14 13:42:07 -0400

    Added lazy adn suspense for handling async component loading

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, lazy, Suspense } from "react";
 import "./App.css";
-import RiverInformation from "./components/RiverInformation/RiverInformation";
+const RiverInformation = lazy(() =>
+  import(/* webpackChunkName: "RiverInformation" */"./components/RiverInformation/RiverInformation")
+);
 
 function App() {
-  const [ river, setRiver ] = useState( "nile" );
-  const [ show, toggle ] = useReducer( (state) => !state, true );
+  const [river, setRiver] = useState("nile");
+  const [show, toggle] = useReducer((state) => !state, true);
   return (
     <div className="wrapper">
       <h1>World's Longest Rivers</h1>
-      <div><button onClick={toggle}>Toggle Details</button></div>
+      <div>
+        <button onClick={toggle}>Toggle Details</button>
+      </div>
       <button onClick={() => setRiver("nile")}>Nile</button>
       <button onClick={() => setRiver("amazon")}>Amazon</button>
       <button onClick={() => setRiver("yangtze")}>Yangtze</button>
       <button onClick={() => setRiver("mississippi")}>Mississippi</button>
-      {show && <RiverInformation name={river} />}
+      <Suspense fallback={<div>Loading Component</div>}>
+        {show && <RiverInformation name={river} />}
+      </Suspense>
     </div>
   );
 }
